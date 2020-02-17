b861cb081906ef50be2dc53d904c9ebfde035f40
Muskaan Singh <47673038+muskaansingh@users.noreply.github.com>
2020-02-17 04:27:53 -0800

    Update createServer.js

--- createServer.js
+++ createServer.js
@@ -3,6 +3,6 @@ const server = http.createServer((request, response) => {
 
     response.end('Nodejs basics');
 });
-server.listen(8081);
+server.listen(3000);
 
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:3000/');
